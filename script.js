805518bc143f520ea9b94d495d13bd993ffbbc39
rileyk94 <rileyk94@gmail.com>
2025-04-30 16:38:38 -0500

    add error catcher for inputs

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ let individualSize = 25
 
 function createGrid() {
     for (let i = 0; i < size; i++) {
-        console.log(size)
         const div = document.createElement('div');
         div.classList.add("grid")
         div.addEventListener("mouseover", () => {
             div.style.backgroundColor = "black";
         })
-        div.style.width = `${individualSize}%`
-        div.style.height = `${individualSize}%`
+        div.style.width = `${individualSize}%`;
+        div.style.height = `${individualSize}%`;
         border.appendChild(div);
         const grid = document.querySelectorAll(".grid")
         reset.addEventListener("click", () => {
@@ -35,16 +34,24 @@ sizeButton.addEventListener("click", () => {
     grid.forEach(div => {
         border.removeChild(div)
     })
-    let side = prompt("Enter a side length (Max 100): ");
-    if (side !== null) {
-        let sideLength = Number(side);
-        console.log(sideLength)
+    let correctInput = false;
+    while (correctInput === false) {
+        let side = prompt("Enter a side length (Max 100): ");
+        if (side !== null) {
+            let sideLength = Number(side);
+            console.log(sideLength)
 
-        if (!isNaN(sideLength)) {
-            size = sideLength*sideLength;
-            individualSize = 100/sideLength;
-            console.log(size)
-        } 
+            if (!isNaN(sideLength)) {
+                if (sideLength <= 100 && sideLength > 0) {
+                    size = sideLength*sideLength;
+                    individualSize = 100/sideLength;
+                    correctInput = true;
+                }
+            } 
+        }
+        if (correctInput === false) {
+            alert("Incorrect Input, please try again")
+        }
     }
     createGrid()
     reset.addEventListener("click", () => {
