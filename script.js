ae6b9db28fb2e4bb58854c0faadaf35f8879b732
rileyk94 <rileyk94@gmail.com>
2025-04-30 16:05:25 -0500

    feat: add user inputed size for the grid

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
 const border = document.querySelector(".container");
-for (let i = 1; i < 17; i++) {
-    const div = document.createElement('div');
-    div.classList.add("grid")
-    draw(div);
-    border.appendChild(div);
+const reset = document.getElementById("reset");
+const sizeButton = document.getElementById("size");
+let size = 16;
+let individualSize = 25
+
+
+
+
+function createGrid() {
+    for (let i = 0; i < size; i++) {
+        console.log(size)
+        const div = document.createElement('div');
+        div.classList.add("grid")
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "black";
+        })
+        div.style.width = `${individualSize}%`
+        div.style.height = `${individualSize}%`
+        border.appendChild(div);
+        const grid = document.querySelectorAll(".grid")
+        reset.addEventListener("click", () => {
+            grid.forEach(div => {
+                div.style.backgroundColor = "aquamarine";
+            })  
+        })
+    }
 }
 
-function draw(square) {
-    square.addEventListener("mouseover", () => {
-        square.classList.add("hover")
+createGrid()
+
+
+sizeButton.addEventListener("click", () => {
+    const grid = document.querySelectorAll(".grid")
+    grid.forEach(div => {
+        border.removeChild(div)
     })
-    square.addEventListener("mouseout", () => {
-        square.classList.remove("hover")
+    let side = prompt("Enter a side length (Max 100): ");
+    if (side !== null) {
+        let sideLength = Number(side);
+        console.log(sideLength)
+
+        if (!isNaN(sideLength)) {
+            size = sideLength*sideLength;
+            individualSize = 100/sideLength;
+            console.log(size)
+        } 
+    }
+    createGrid()
+    reset.addEventListener("click", () => {
+        grid.forEach(div => {
+            div.style.backgroundColor = "aquamarine";
+        })  
     })
-}
+})
+
+
